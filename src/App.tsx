5b399d8fd0ca977cb2ff628bdcd95bbd096827fa
Anteneh Solomon <antenehcs@gmail.com>
2023-01-18 10:21:58 +0300

    sidenav

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import SideNav from './widgets/layouts/sideNav';
 
 function App() {
   return (
     <div className="App">
-      
+      <SideNav/>
     </div>
   );
 }
