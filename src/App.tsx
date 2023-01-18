2d2aebe63fc75aca22c5b8fec1ffdd73a1b12678
Anteneh Solomon <antenehcs@gmail.com>
2023-01-18 10:51:01 +0300

    AIS initialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import SideNav from './widgets/layouts/sideNav';
 
 function App() {
   return (
     <div className="App">
-      <SideNav/>
+      
     </div>
   );
 }
