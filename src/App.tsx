d8401b05f82f04c6465e362e927dfc1cfb6ee6de
Anteneh Solomon <antenehcs@gmail.com>
2023-01-24 14:59:11 +0300

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
 import React from 'react';
 import './App.css';
-import Sidebar from './widgets/layouts/SideBar';
-import NavBar from './widgets/layouts/NavBar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import Dashboard from './components/Dashboard'
-import Course from './components/Course'
-import Profile from './components/Profile'
-import Login from './components/Login'
+import Login from './components/Login';
+
 
 
 function App() {
   
   return (
     <div className="App">
-      <BrowserRouter>
-      <NavBar/>
-      <Sidebar/>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/dashboard' element={<Dashboard />} />
-                <Route path='/course' element={<Course />} />
-                <Route path='/profile' element={<Profile />} />
-                <Route path='/login' element={<Login />} />
-            </Routes>
-        </BrowserRouter>
+      <Login/>
     </div>
   );
 }
