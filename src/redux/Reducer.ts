81093bd4e5993ad47f075dddc5ea7309b2b8521d
Anteneh Solomon <antenehcs@gmail.com>
2023-03-04 10:59:29 +0300

    reducer

---
+++ src/redux/Reducer.ts
@@ -0,0 +1,14 @@
+import { ADD_BREADCRUMB } from "./ActionTypes";
+
+const initialStateBreadCrumb:any[] = [];
+
+export const breadcrumbReducers = ( state=initialStateBreadCrumb, action:any ) => {
+    const { type,payload } = action;
+    switch(type){
+        case ADD_BREADCRUMB:
+            return[payload];
+        default:
+            return state;
+
+    }
+}
