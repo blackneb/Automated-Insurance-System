5bc381e7fac10c6c116b89739e23bae4dfb2a356
Anteneh Solomon <antenehcs@gmail.com>
2023-03-09 16:36:38 +0300

    total garages table

--- src/components/Garages.tsx
+++ src/components/Garages.tsx
@@ -1,9 +1,13 @@
 import React,{ useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { add_breadcrumb } from '../redux/Actions';
+import TotalGarages from './Tables/TotalGarages';
+import {garages} from '../data/garages';
+
 
 const Garages = () => {
     const dispatch = useDispatch();
+    const data:any[] = garages;
   const breadcrumb:any[] = [
     {title:"Home",path:"/"},
     {title:"Garages",path:"/garage"},
@@ -13,7 +17,7 @@ const Garages = () => {
   },[])
   return (
     <div className='mt-4 ml-4'>
-      Garages
+      <TotalGarages data={data}/>
     </div>
   )
 }
--- src/components/Tables/TotalGarages.tsx
+++ src/components/Tables/TotalGarages.tsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react';
+import type { TableProps } from 'antd';
+import { Button, Space, Table,Input } from 'antd';
+import type { ColumnsType, FilterValue, SorterResult } from 'antd/es/table/interface';
+const { Search } = Input;
 
-const TotalGarages = () => {
+interface DataType{
+  ID:string,
+  garageName:string,
+  phoneNumber:string,
+  email:string,
+  address:string,
+  documents:string,
+}
+
+const TotalGarages = ({data}:any) => {
+  const [sortedInfo, setSortedInfo] = useState<SorterResult<DataType>>({});
+  const [searchValue, setSearchValue] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    console.log(e.target.value);
+    setSearchValue(e.target.value.toLowerCase());
+  };
+  
+
+  const handleChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter) => {
+    console.log('Various parameters', pagination, filters, sorter);
+    setSortedInfo(sorter as SorterResult<DataType>);
+  };
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: 'Garage Name',
+      dataIndex: 'garageName',
+      key: 'garageName',
+      sorter: (a, b) => a.garageName.length - b.garageName.length,
+      sortOrder: sortedInfo.columnKey === 'garageName' ? sortedInfo.order : null,
+      ellipsis: true,
+    },
+    {
+      title: 'Phone Number',
+      dataIndex: 'phoneNumber',
+      key: 'phoneNumber',
+      sorter: (a, b) => a.phoneNumber.length - b.phoneNumber.length,
+      sortOrder: sortedInfo.columnKey === 'phoneNumber' ? sortedInfo.order : null,
+      ellipsis: true,
+    },
+    {
+      title: 'E-mail',
+      dataIndex: 'email',
+      key: 'email',
+      sorter: (a, b) => a.email.length - b.email.length,
+      sortOrder: sortedInfo.columnKey === 'email' ? sortedInfo.order : null,
+      ellipsis: true,
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+      sorter: (a, b) => a.address.length - b.address.length,
+      sortOrder: sortedInfo.columnKey === 'address' ? sortedInfo.order : null,
+      ellipsis: true,
+    },
+    {
+      title: 'Documents',
+      dataIndex: 'documents',
+      key: 'documents',
+      sorter: (a, b) => a.documents.length - b.documents.length,
+      sortOrder: sortedInfo.columnKey === 'files' ? sortedInfo.order : null,
+      ellipsis: true,
+    },
+  ];
   return (
-    <div>
-      
+    <div className='mx-4 mt-4 bg-white shadow rounded-md border-0 p-2 shadow'>
+      <p>Total Garages</p>
+      <Input className='mb-2' placeholder="Search With Client Name" allowClear onChange={onChange} />
+      <Table columns={columns} dataSource={data.filter((items:any) => items.garageName.toLowerCase().includes(searchValue))} onChange={handleChange} />
     </div>
   )
 }
