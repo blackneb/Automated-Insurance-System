6bdfc623ef8e43a0064ae5ac7c1d0af4b091264c
blackneb <antenehcs@gmail.com>
2023-03-26 13:29:31 +0300

    extra fitting

--- src/components/New Vehicle Insurance/ExtraFitting.tsx
+++ src/components/New Vehicle Insurance/ExtraFitting.tsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { Button, Checkbox, Form, Input, Upload} from 'antd';
 
 const ExtraFitting = () => {
-  return (
-    <div>
+    const onFinish = (values: any) => {
+        console.log('Success:', values);
+      };
       
+      const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+      };
+  return (
+    <div className='flex justify-center my-8'>
+       <Form
+        name=""
+        labelCol={{ flex: '300px' }}
+        labelAlign="left"
+        labelWrap
+        colon={false}
+      >
+        <Form.Item
+            label="Radios, Tape Recorders and record Players ?"
+            name="radio"
+            rules={[{ required: true, message: 'Please input your Chassis Number!' }]}
+            >
+            <Input />
+        </Form.Item>
+        <Form.Item
+            label="Communication Equipment ?"
+            name="communication"
+            rules={[{ required: true, message: 'Please input your Chassis Number!' }]}
+            >
+            <Input />
+        </Form.Item>
+        <Form.Item
+            label="Bull bar, carry boy, droppers extension ?"
+            name="BCD"
+            rules={[{ required: true, message: 'Please input your Chassis Number!' }]}
+            >
+            <Input />
+        </Form.Item>
+
+      </Form>
     </div>
   )
 }
