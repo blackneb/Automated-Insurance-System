76b755b80302baa06f8ea0c5776de9a7e2d8fe49
blackneb <antenehcs@gmail.com>
2023-03-28 14:27:06 +0300

    user history

--- src/components/UserHistory.tsx
+++ src/components/UserHistory.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Divider, Steps } from 'antd';
+
 
 const UserHistory = () => {
   return (
-    <div>
-      
+    <div className='mt-4 ml-16 h-screen'>
+      <Divider />
+        <Steps
+          progressDot
+          current={0}
+          direction="vertical"
+          items={[
+            {
+              title: 'Date 14-08-22',
+              description: 'This is a description. This is a description.',
+            },
+            {
+              title: 'Date 14-08-22',
+              description: 'This is a description. This is a description.',
+            },
+            {
+              title: 'Date 14-08-22',
+              description: 'This is a description. This is a description.',
+            },
+            {
+              title: 'Date 14-08-22',
+              description: 'This is a description.',
+            },
+            {
+              title: 'Date 14-08-22',
+              description: 'This is a description.',
+            },
+          ]}
+        />
     </div>
   )
 }
