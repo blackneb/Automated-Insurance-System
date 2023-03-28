36493b3ebb3af54a32c6a7f412e216aeeaf42303
blackneb <antenehcs@gmail.com>
2023-03-28 13:14:09 +0300

    expert progress

--- src/components/ExpertProgress.tsx
+++ src/components/ExpertProgress.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import { add_breadcrumb } from '../redux/Actions';
+import {progress} from '../data/progress';
+import ClaimProgress from './Tables/ClaimProgress';
 
 const ExpertProgress = () => {
+  const dispatch = useDispatch();  
+  const breadcrumb:any[] = [
+    {title:"Home",path:"/"},
+    {title:"Progress",path:"/vehicle"},
+  ]
+  const data:any[] = progress;
+  useEffect(() => {
+    dispatch(add_breadcrumb(breadcrumb));
+  },[])
   return (
-    <div>
-      
+    <div className='mt-4 ml-4 h-screen'>
+      <ClaimProgress data={data}/>
     </div>
   )
 }
