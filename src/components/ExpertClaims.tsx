093d641a4caa1cf7ee11ff78c6f5fc9a97e40016
blackneb <antenehcs@gmail.com>
2023-03-29 13:47:28 +0300

    active claims

--- src/components/ExpertClaims.tsx
+++ src/components/ExpertClaims.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import { add_breadcrumb } from '../redux/Actions';
+import {progress} from '../data/progress';
+import ActiveClaims from './Tables/ActiveClaims';
 
 const ExpertClaims = () => {
+  const dispatch = useDispatch();  
+  const breadcrumb:any[] = [
+    {title:"Home",path:"/"},
+    {title:"claims",path:"/userclaims"},
+  ]
+  const data:any[] = progress;
+  useEffect(() => {
+    dispatch(add_breadcrumb(breadcrumb));
+  },[])
   return (
-    <div>
-      
+    <div className='mt-4 ml-4 h-screen'>
+      <ActiveClaims data={data}/>
     </div>
   )
 }
--- src/components/Tables/ActiveClaims.tsx
+++ src/components/Tables/ActiveClaims.tsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, {useState} from 'react'
+import type { TableProps } from 'antd';
+import { Button, Space, Table,Input } from 'antd';
+import { Progress } from 'antd';
+import type { ColumnsType, FilterValue, SorterResult } from 'antd/es/table/interface';
+import {vehicles} from '../../data/vehicles';
+const { Search } = Input;
 
-const ActiveClaims = () => {
+interface DataType {
+  claimDate: string;
+  accidentId: string;
+  progress:string;
+  proposer:string;
+  key:string
+}
+
+const ActiveClaims = ({data}:any) => {
+  const [sortedInfo, setSortedInfo] = useState<SorterResult<DataType>>({});
+  const [searchValue, setSearchValue] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    console.log(e.target.value);
+    setSearchValue(e.target.value);
+  };
+  
+
+  const handleChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter) => {
+    console.log('Various parameters', pagination, filters, sorter);
+    setSortedInfo(sorter as SorterResult<DataType>);
+  };
+  const columns: ColumnsType<DataType> = [
+    {
+      title: 'Date',
+      dataIndex: 'claimDate',
+      key: 'claimDate',
+      sorter: (a, b) => a.claimDate.length - b.claimDate.length,
+      sortOrder: sortedInfo.columnKey === 'claimDate' ? sortedInfo.order : null,
+      ellipsis: true,
+    },
+    {
+      title: 'Accident ID',
+      dataIndex: 'accidentId',
+      key: 'accidentId',
+      sorter: (a, b) => a.accidentId.length - b.accidentId.length,
+      sortOrder: sortedInfo.columnKey === 'accidentId' ? sortedInfo.order : null,
+      ellipsis: true,
+    },
+    {
+      title: 'Vehcile Owner',
+      dataIndex: 'proposer',
+      key: 'proposer',
+      sorter: (a, b) => a.proposer.length - b.proposer.length,
+      sortOrder: sortedInfo.columnKey === 'proposer' ? sortedInfo.order : null,
+      ellipsis: true,
+    },
+    {
+      title: 'Progress',
+      dataIndex: '',
+      key: 'x',
+      render: () => <div><Progress percent={100} status="active" /></div> ,
+    },
+    {
+        title: 'Action',
+        dataIndex: '',
+        key: 'x',
+        render: () => <div><a>View</a></div> ,
+      },
+  ];
   return (
-    <div>
-      
+    <div className='mx-4 mt-4 bg-white shadow rounded-md border-0 p-2 shadow'>
+      <p>New Claims</p>
+      <Input className='mb-2' placeholder="Search with Date" allowClear onChange={onChange} />
+      <Table columns={columns} dataSource={data.filter((items:any) => items.proposer.includes(searchValue))} onChange={handleChange} />
     </div>
   )
 }
