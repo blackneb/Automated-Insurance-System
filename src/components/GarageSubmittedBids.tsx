37980450e57aef85fcdb3d6e82f26acf9b0e3435
blackneb <antenehcs@gmail.com>
2023-03-29 13:17:55 +0300

    update

--- src/components/GarageSubmittedBids.tsx
+++ src/components/GarageSubmittedBids.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React,{ useEffect } from 'react'
+import { Card, Space } from 'antd';
+import { useDispatch } from 'react-redux';
+import { add_breadcrumb } from '../redux/Actions';
+import { garagesubmittedbids } from '../data/garagesubmittedbids';
+import GarageSubBids from './Tables/GarageSubBids';
 const GarageSubmittedBids = () => {
+  const dispatch = useDispatch();
+  const data:any[] = garagesubmittedbids;
+  const breadcrumb:any[] = [
+    {title:"Home",path:"/"},
+    {title:"Submitted Bids",path:"/garage"},
+  ]
+  useEffect(() => {
+    dispatch(add_breadcrumb(breadcrumb));
+  },[])
   return (
-    <div>
-      
+    <div className='mt-4 ml-4 h-screen'>
+      <GarageSubBids data={data}/>
     </div>
   )
 }
---
+++ src/components/Tables/GarageSubBids.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import type { TableProps } from 'antd';
+import { Button, Space, Table,Input } from 'antd';
+import type { ColumnsType, FilterValue, SorterResult } from 'antd/es/table/interface';
+const { Search } = Input;
+
+interface DataType{
+    date:string,
+    vehicleId:string,
+    itemsNumber:string,
+    totalAmount:string,
+    ownerName:string,
+  }
+
+const GarageSubBids = ({data}:any) => {
+  const [sortedInfo, setSortedInfo] = useState<SorterResult<DataType>>({});
+  const [searchValue, setSearchValue] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    console.log(e.target.value);
+    setSearchValue(e.target.value.toLowerCase());
+  };
+  
+
+  const handleChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter) => {
+    console.log('Various parameters', pagination, filters, sorter);
+    setSortedInfo(sorter as SorterResult<DataType>);
+  };
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: 'Date',
+      dataIndex: 'date',
+      key: 'date',
+      sorter: (a, b) => a.date.length - b.date.length,
+      sortOrder: sortedInfo.columnKey === 'date' ? sortedInfo.order : null,
+      ellipsis: true,
+    },
+    {
+      title: 'Vehicle ID',
+      dataIndex: 'vehicleId',
+      key: 'vehicleId',
+      sorter: (a, b) => a.vehicleId.length - b.vehicleId.length,
+      sortOrder: sortedInfo.columnKey === 'vehicleId' ? sortedInfo.order : null,
+      ellipsis: true,
+    },
+    {
+      title: 'Items Number',
+      dataIndex: 'itemsNumber',
+      key: 'itemsNumber',
+      sorter: (a, b) => a.itemsNumber.length - b.itemsNumber.length,
+      sortOrder: sortedInfo.columnKey === 'itemsNumber' ? sortedInfo.order : null,
+      ellipsis: true,
+    },
+    {
+      title: 'Total Amount',
+      dataIndex: 'totalAmount',
+      key: 'totalAmount',
+      sorter: (a, b) => a.totalAmount.length - b.totalAmount.length,
+      sortOrder: sortedInfo.columnKey === 'totalAmount' ? sortedInfo.order : null,
+      ellipsis: true,
+    },
+    {
+      title: 'Owner Name',
+      dataIndex: 'ownerName',
+      key: 'ownerName',
+      sorter: (a, b) => a.ownerName.length - b.ownerName.length,
+      sortOrder: sortedInfo.columnKey === 'ownerName' ? sortedInfo.order : null,
+      ellipsis: true,
+    },
+  ];
+  return (
+    <div className='mx-4 mt-4 bg-white shadow rounded-md border-0 p-2 shadow'>
+      <p>Submitted Bids</p>
+      <Input className='mb-2' placeholder="Search With Vehicle ID" allowClear onChange={onChange} />
+      <Table columns={columns} dataSource={data.filter((items:any) => items.vehicleId.toLowerCase().includes(searchValue))} onChange={handleChange} />
+    </div>
+  )
+}
+
+export default GarageSubBids
