aac07307feef4ab26d0961c7d7d4544b80bba7ae
blackneb <antenehcs@gmail.com>
2023-03-30 10:07:35 +0300

    actions

--- src/redux/Actions.ts
+++ src/redux/Actions.ts
@@ -1,8 +1,29 @@
-import { ADD_BREADCRUMB } from "./ActionTypes";
+import { ADD_BREADCRUMB, ADD_BASIC_INFORMATION, ADD_ADDRESS, ADD_ACCOUNT_SET_UP } from "./ActionTypes";
 
 export const add_breadcrumb = (breadcrumb:any) => {
     return{
         type:ADD_BREADCRUMB,
         payload:breadcrumb,
+    }
+};
+
+export const add_basic_information = (basicinformation:any) => {
+    return{
+        type:ADD_BASIC_INFORMATION,
+        payload:basicinformation,
+    }
+};
+
+export const add_address = (address:any) => {
+    return{
+        type:ADD_ADDRESS,
+        payload:address,
+    }
+};
+
+export const add_account_set_up = (accountsetup:any) => {
+    return{
+        type:ADD_ACCOUNT_SET_UP,
+        payload:accountsetup,
     }
 };
