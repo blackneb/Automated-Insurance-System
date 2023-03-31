e2a561ecc3a841de9869126fa315fd99ab430e34
blackneb <antenehcs@gmail.com>
2023-03-31 09:35:11 +0300

    actions

--- src/redux/Actions.ts
+++ src/redux/Actions.ts
@@ -1,4 +1,4 @@
-import { ADD_BREADCRUMB, ADD_BASIC_INFORMATION, ADD_ADDRESS, ADD_ACCOUNT_SET_UP } from "./ActionTypes";
+import { ADD_BREADCRUMB, ADD_BASIC_INFORMATION, ADD_ADDRESS, ADD_ACCOUNT_SET_UP, ADD_PARTICULAR, ADD_EXTRA_FITTING, ADD_OTHER_INSURANCE, ADD_ACCIDENT_BEFORE, ADD_ADDITIONAL_INFO, ADD_DATE_APPOINTMENT } from "./ActionTypes";
 
 export const add_breadcrumb = (breadcrumb:any) => {
     return{
@@ -26,4 +26,46 @@ export const add_account_set_up = (accountsetup:any) => {
         type:ADD_ACCOUNT_SET_UP,
         payload:accountsetup,
     }
+};
+
+export const add_particular = (particular:any) => {
+    return{
+        type:ADD_PARTICULAR,
+        payload:particular,
+    };
+};
+
+export const add_extra_fitting = (extrafitting:any) => {
+    return{
+        type:ADD_EXTRA_FITTING,
+        payload:extrafitting,
+    };
+};
+
+export const add_other_insurance = (otherinsurance:any) => {
+    return{
+        type:ADD_OTHER_INSURANCE,
+        payload:otherinsurance,
+    };
+};
+
+export const add_accident_before = (accidentbefore:any) => {
+    return{
+        type:ADD_ACCIDENT_BEFORE,
+        payload:accidentbefore,
+    };
+};
+
+export const add_additional_info = (additionalinfo:any) => {
+    return{
+        type:ADD_ADDITIONAL_INFO,
+        payload:additionalinfo,
+    };
+};
+
+export const add_date_appointment = (dateappointment:any) => {
+    return{
+        type:ADD_DATE_APPOINTMENT,
+        payload:dateappointment,
+    };
 };
