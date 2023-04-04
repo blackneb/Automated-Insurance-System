2c77ff9665d5ee31225d09ba3e4c47f5f906a728
blackneb <antenehcs@gmail.com>
2023-04-04 15:46:58 +0300

    claim

---
+++ src/components/Modals/HealthClaimModal.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HealthClaimModal = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HealthClaimModal
--- src/components/UserClaims.tsx
+++ src/components/UserClaims.tsx
@@ -1,12 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { add_breadcrumb } from '../redux/Actions';
 import {progress} from '../data/progress';
 import ClaimProgress from './Tables/ClaimProgress';
 import UsersMyClaims from './Tables/UsersMyClaims';
+import VehicleClaimModal from './Modals/VehicleClaimModal';
+import HealthClaimModal from './Modals/HealthClaimModal';
+import { Button, Modal } from 'antd';
 
 const UserClaims = () => {
-  const dispatch = useDispatch();  
+  const dispatch = useDispatch();
+  const [openModalVehicle, setOpenModalVehicle] = useState(false);
+  const [openModalHealth, setOpenModalHealth] = useState(false);  
   const breadcrumb:any[] = [
     {title:"Home",path:"/"},
     {title:"claims",path:"/userclaims"},
@@ -17,6 +22,30 @@ const UserClaims = () => {
   },[])
   return (
     <div className='mt-4 ml-4 h-screen'>
+      <div className='flex flex-row justify-evenly'>
+      <Modal
+        title="Claim For Vehicle"
+        style={{ top: 20 }}
+        open={openModalVehicle}
+        onOk={() => setOpenModalVehicle(false)}
+        onCancel={() => setOpenModalVehicle(false)}
+        width={1200}
+      >
+        <VehicleClaimModal />
+      </Modal>
+      <Modal
+        title="Claim For Health"
+        style={{ top: 20 }}
+        open={openModalHealth}
+        onOk={() => setOpenModalHealth(false)}
+        onCancel={() => setOpenModalHealth(false)}
+        width={1200}
+      >
+        < HealthClaimModal />
+      </Modal>
+        <Button onClick={()=>setOpenModalVehicle(true)}>Vehicle Claim</Button>
+        <Button onClick={()=>setOpenModalHealth(true)}>Health Claim</Button>
+      </div>
       <UsersMyClaims data={data}/>
     </div>
   )
