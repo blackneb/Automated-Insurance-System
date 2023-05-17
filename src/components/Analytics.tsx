d6cd9a5b66da73f48ee99cce9a45d3699999eb90
blackneb <antenehcs@gmail.com>
2023-05-17 10:34:02 +0300

    age accident chart

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -6,6 +6,18 @@ import ChartUsersStatus from './Charts/ChartUsersStatus';
 import ChartVehicleBrand from './Charts/ChartVehicleBrand';
 import ClaimsChartYearly from './Charts/ClaimsChartYearly';
 import MonthlyExpenduters from './Charts/MonthlyExpenduters';
+import CoverRequiredChart from './Charts/CoverRequiredChart';
+import VehiclePurposeChart from './Charts/VehiclePurposeChart';
+import HorsePowerChart from './Charts/HorsePowerChart';
+import ExtraFittingChart from './Charts/ExtraFittingChart';
+import AccidentTypesChart from './Charts/AccidentTypesChart';
+import YearAccidentChart from './Charts/YearAccidentChart';
+import VehicleSpeedAccidentChart from './Charts/VehicleSpeedAccidentChart';
+import RoadTypesAccidentChart from './Charts/RoadTypesAccidentChart';
+import GenderAccidentChart from './Charts/GenderAccidentChart';
+import CityAccidentChart from './Charts/CityAccidentChart';
+import DeathChart from './Charts/DeathChart';
+import AgeAccidentChart from './Charts/AgeAccidentChart';
 
 const Analytics = () => {
   const dispatch = useDispatch();
@@ -134,61 +146,73 @@ const Analytics = () => {
             case "CoverRequiredChart":
               return(
                 <div>
+                  <CoverRequiredChart/>
                 </div>
               )
             case "VehiclePurposeChart":
               return(
                 <div>
+                  <VehiclePurposeChart/>
                 </div>
               )
             case "HorsePowerChart":
               return(
                 <div>
+                  <HorsePowerChart/>
                 </div>
               )
             case "ExtraFittingChart":
               return(
                 <div>
+                  <ExtraFittingChart/>
                 </div>
               )
             case "AccidentTypesChart":
               return(
                 <div>
+                  <AccidentTypesChart/>
                 </div>
               )
             case "AgeAccidentChart":
               return(
                 <div>
+                  <AgeAccidentChart/>
                 </div>
               )
             case "YearAccidentChart":
               return(
                 <div>
+                  <YearAccidentChart/>
                 </div>
               )
             case "VehicleSpeedAccidentChart":
               return(
                 <div>
+                  <VehicleSpeedAccidentChart/>
                 </div>
               )
             case "RoadTypesAccidentChart":
               return(
                 <div>
+                  <RoadTypesAccidentChart/>
                 </div>
               )
             case "GenderAccidentChart":
               return(
                 <div>
+                  <GenderAccidentChart/>
                 </div>
               )
             case "CityAccidentChart":
               return(
                 <div>
+                  <CityAccidentChart/>
                 </div>
               )
             case "DeathChart":
               return(
                 <div>
+                  <DeathChart/>
                 </div>
               )          
           }
